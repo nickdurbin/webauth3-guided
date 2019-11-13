aaa3847d5d12b28e689d805094c25900f312d04f
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-11-13 11:17:12 +0100

    wip

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,23 +1,20 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
+  const token = req.headers.authorization;
+  if (token) {
+    // check token, if good, just next()
+    jwt.verify(
+      token,
+      'THIS IS THE SECRET',
+      (err, decodedToken) => {
+        if (err) {
+          // this would mean the token is bad
         } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
+          // happy path
         }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+      }
+    )
   } else {
     res.status(400).json({ message: 'No credentials provided' });
   }
