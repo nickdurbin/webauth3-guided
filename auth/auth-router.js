5e2773a97fe2ba1496de5e492012f274cde6a970
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-11-13 10:54:45 +0100

    wip

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -26,8 +26,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // THIS HERE IS THE PLACE TO MAKE THE TOKEN
+        const token = generateToken(user);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token: token,
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
